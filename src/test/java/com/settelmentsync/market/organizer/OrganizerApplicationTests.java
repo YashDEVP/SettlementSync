42ed3dee613d126af034ac3241bac54f8250a0e2
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-27 00:04:25 -0700

    Project setup code

---
+++ src/test/java/com/settelmentsync/market/organizer/OrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.settelmentsync.market.organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
